7b80df8031687fa019316c6f83c86e5fad6dfec3
avcopan <avcopan@gmail.com>
2024-07-22 09:33:00 -0500

    WIP: Activates Pixi environment over SSH

--- examples/submit.sh
+++ examples/submit.sh
@@ -11,14 +11,19 @@ WORK_DIR="$(cd "$(dirname ${WORK_DIR})"; pwd)/$(basename "${WORK_DIR}")"
 
 function run_job() {
     local work_dir=${1}
-    local num_parallel=${2:-1}
+    local manifest_file=${2}
+    local num_parallel=${3}
+    export PATH=${HOME}/.pixi/bin:${PATH}
     cd ${work_dir}
     echo "Running ${num_parallel} instances on $(hostname) at ${PWD}"
-    source ~/.bash_profile
+    which pixi
+    eval "$(pixi shell-hook --manifest-path=${manifest_file})"
+    which python
+    which automech
     # for i in $(seq ${num_parallel}); do
     #     pixi run automech run &> "out${i}.log" &
     # done
 }
 
 # ssh ${HOST}  'bash -s' < ${SCRIPT} ${WORK_DIR}
-ssh ${HOST} "$(declare -f run_job); run_job ${WORK_DIR} ${NUM_PARALLEL}"
+ssh ${HOST} "$(declare -f run_job); run_job ${WORK_DIR} ${MANIFEST_FILE} ${NUM_PARALLEL}"
